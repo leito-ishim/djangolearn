26d04ee9f3fe40c5bbb662e0fa341ba09d43fe29
Admin <leito-ishim@mail.ru>
2021-11-29 22:33:55 +0500

    add ajas for basket_add

--- geekshop/static/js/basket.js
+++ geekshop/static/js/basket.js
@@ -13,4 +13,18 @@ window.onload = function () {
         event.preventDefault()
 
     })
+
+    $('.card_add_basket').on('click', 'button[type="button"]', function () {
+        let t_href = event.target.value
+        $.ajax(
+            {
+                url: "/baskets/add/" + t_href + "/",
+                success: function (data) {
+                    $('.card_add_basket').html(data.result)
+                },
+        });
+        event.preventDefault()
+
+    })
+
 }
