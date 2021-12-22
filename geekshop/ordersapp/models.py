ff0144f6b236aa82b5befd678fd712cd6a4e0ac7
Admin <leito-ishim@mail.ru>
2021-12-23 00:28:01 +0500

    comlete tasks for lesson 3 part 2

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -23,8 +23,8 @@ class Order(models.Model):
     )
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    create = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
-    update = models.DateTimeField(verbose_name='дата обновления', auto_now=True)
+    created = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='дата обновления', auto_now=True)
     status = models.CharField(choices=ORDER_STATUS_CHOICES, verbose_name='статус', max_length=3, default=FORMING)
     is_active = models.BooleanField(verbose_name='активный', default=True)
 
